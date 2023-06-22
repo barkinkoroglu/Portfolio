3329fcde143e7e7a9d951fe39749b174de3bdba6
Barkin Koroglu <barkin_45_99@hotmail.com>
2023-06-22 13:58:47 +0300

    screen size

--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -6,7 +6,23 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    maxWidth: {
+      container: '1440px',
+      contentContainer: '1140px',
+      containerSmall: '1024px',
+      containerxs: '768px',
+    },
     extend: {
+      screens: {
+        xs: '320px',
+        sm: '375px',
+        sml: '500px',
+        md: '667px',
+        mdl: '768px',
+        lg: '960px',
+        lgl: '1024px',
+        xl: '1280px',
+      },
       fontFamily: {
         bodyFont: ['Montserrat', 'sans-serif'],
         titleFont: ['Open Sans', 'sans-serif'],
