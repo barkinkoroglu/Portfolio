6b2639bc3d9f51932969d02492ebaddac9396d05
Barkin Koroglu <barkin_45_99@hotmail.com>
2023-06-22 13:55:36 +0300

    initial

---
+++ portfolio/pages/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="font-titleFont">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, qui.
+      Molestiae architecto, sit, dolore omnis praesentium fuga soluta odio esse
+      fugit veniam doloribus eos ad placeat enim repellendus. Provident,
+      cupiditate.
+    </div>
+  );
+}
+
+export default Home;
